743bd1d00ecd28116d538ce8b2bdecd179d2ae44
FranciscoJavierAmaroRodriguez <pacoempires@outlook.com>
2024-05-30 00:55:34 -0500

    4.0

--- Proyecto/consultas.js
+++ Proyecto/consultas.js
@@ -26,7 +26,7 @@ const swaggerOptions = {
             {url: "http://localhost:3000"}, {url: "https://proyectoamaro-production.up.railway.app/"}
         ], 
     },
-    apis: [`${path.join(__dirname,"./Consultas.js")}`],
+    apis: [`${path.join(__dirname,"./consultas.js")}`],
     };
 
 const swaggerDocs = swaggerJsDoc(swaggerOptions);
