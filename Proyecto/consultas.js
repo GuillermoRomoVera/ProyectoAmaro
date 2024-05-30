562c408d4120fb4b095ccaf231f2c49a5a3bfc09
FranciscoJavierAmaroRodriguez <pacoempires@outlook.com>
2024-05-29 19:06:50 -0500

    a

--- Proyecto/consultas.js
+++ Proyecto/consultas.js
@@ -10,7 +10,7 @@ const theme = new SwaggerTheme();
 const options = {
     explorer: true,
     customCss: theme.getBuffer(SwaggerThemeNameEnum.DARK)
-  };
+};
 
 const swaggerOptions = {
     definition: {
@@ -19,102 +19,135 @@ const swaggerOptions = {
             title: 'API D&D',
             version: '1.0.0',
             description: 'Api que realiza un registro simple de personajes para el juego de mesa de D&D'
+        },
+        servers: [
+            { url: "http://localhost:3000" }
+        ],
     },
-        servers:[
-            {url: "http://localhost:3000"}
-        ], 
-    },
-    apis: [`${path.join(__dirname,"./consultas.js")}`],
-    };
+    apis: [`${path.join(__dirname, "./consultas.js")}`],
+};
 
 const swaggerDocs = swaggerJsDoc(swaggerOptions);
-app.use("/api-docs",swaggerUI.serve,swaggerUI.setup(swaggerDocs,options));
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDocs, options));
 
-app.use("/api-docs-json",(req,res)=>{
+app.use("/api-docs-json", (req, res) => {
     res.json(swaggerDocs);
-})
+});
 
 app.use(express.json());
 
 /**
  * @swagger
  * tags:
- * - name: Personajes
+ *   name: Personajes
  *   description: Procesos de Alumnos
- * paths:
- *   /lista:
- *     get:
- *       tags:     
+ * /lista:
+ *   get:
+ *     tags:     
  *       - Personajes
- *       description: Lista de alumnos
- *       responses:
- *         200:
- *           description: Regresa a alumnos.
- *           type: json
+ *     description: Lista de alumnos
+ *     responses:
+ *       200:
+ *         description: Regresa a alumnos.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 type: object
  */
-//Consulta
-app.get("/lista", async (req,res,next)=>{
-    sql ='SELECT * FROM personajes';
-    if (typeof req.query.id !='undefined'){
-    sql = sql + ` where id = "${req.query.id}"`;
+app.get("/lista", async (req, res, next) => {
+    let sql = 'SELECT * FROM personajes';
+    if (req.query.id) {
+        sql += ' WHERE id = ?';
     }
 
-    try{
-    connection =await mysql.createConnection({ host: 'roundhouse.proxy.rlwy.net', user:'root', database: 'railway', password: 'BJeuxTjJuwfhXSaolTreiTJqpiREPhAls'});
-    var [rows,fields]=await connection.query(sql);
-    connection.end();
-    
-    (rows.length > 0) ? res.send(rows) : res.status(404).json({Eror: "Datos no encontrados"});
+    try {
+        const connection = await mysql.createConnection({
+            host: 'roundhouse.proxy.rlwy.net',
+            user: 'root',
+            database: 'railway',
+            password: 'BJeuxTjJuwfhXSaolTreiTJqpiREPhAl'
+        });
 
-}
-catch(err){
-        res.send(err.code+` / `+err.message);
+        const [rows, fields] = await connection.execute(sql, [req.query.id]);
+        connection.end();
+
+        if (rows.length > 0) {
+            res.send(rows);
+        } else {
+            res.status(404).json({ error: "Datos no encontrados" });
+        }
+    } catch (err) {
+        res.send(err.code + ` / ` + err.message);
     }
 });
 
-//Update
+// Update
 app.put("/actualizar", async (req, res) => {
     try {
-        connection = await mysql.createConnection({ host: 'roundhouse.proxy.rlwy.net', user: 'root', database: 'railway', password: 'BJeuxTjJuwfhXSaolTreiTJqpiREPhAl' });
-        sql =`UPDATE personajes SET id ="${req.query.idNuevo}"` + ` where id = "${req.query.id}"`;
-        result= await connection.query(sql);
+        const connection = await mysql.createConnection({
+            host: 'roundhouse.proxy.rlwy.net',
+            user: 'root',
+            database: 'railway',
+            password: 'BJeuxTjJuwfhXSaolTreiTJqpiREPhAl'
+        });
+
+        const sql = 'UPDATE personajes SET id = ? WHERE id = ?';
+        const [result] = await connection.execute(sql, [req.query.idNuevo, req.query.id]);
         await connection.end();
-        if (result.affectedRows !==0) {
-            res.status(200).json({ message: "Nombre actualizado con exito" });
+
+        if (result.affectedRows !== 0) {
+            res.status(200).json({ message: "Nombre actualizado con éxito" });
         } else {
             res.status(404).json({ error: "No se encontró el nombre" });
         }
-    } catch(err){
-        res.send(err.code+` / `+err.message);
+    } catch (err) {
+        res.send(err.code + ` / ` + err.message);
     }
 });
 
-//Añadir
+// Añadir
 app.post("/agregar", async (req, res) => {
     try {
-        connection = await mysql.createConnection({ host: 'roundhouse.proxy.rlwy.net', user: 'root', database: 'railway', password: 'BJeuxTjJuwfhXSaolTreiTJqpiREPhAl' });
-        sql =`INSERT INTO personajes (Nombre, Raza, Clase, Fuerza, Destreza, Constitucion, Inteligencia, Sabiduria, Carisma) VALUES ("${req.query.Nombre}","${req.query.Raza},"${req.query.Clase},"${req.query.Fuerza}","${req.query.Destreza}","${req.query.Constitucion}","${req.query.Inteligencia}","${req.query.Sabiduria}","${req.query.Carisma}")`;
-        var [rows,fields] = await connection.query(sql);
+        const connection = await mysql.createConnection({
+            host: 'roundhouse.proxy.rlwy.net',
+            user: 'root',
+            database: 'railway',
+            password: 'BJeuxTjJuwfhXSaolTreiTJqpiREPhAl'
+        });
+
+        const sql = 'INSERT INTO personajes (Nombre, Raza, Clase, Fuerza, Destreza, Constitucion, Inteligencia, Sabiduria, Carisma) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)';
+        const values = [req.query.Nombre, req.query.Raza, req.query.Clase, req.query.Fuerza, req.query.Destreza, req.query.Constitucion, req.query.Inteligencia, req.query.Sabiduria, req.query.Carisma];
+        const [rows, fields] = await connection.execute(sql, values);
         await connection.end();
+
         res.status(201).json({ message: "Nuevo Personaje agregado correctamente" });
-    } catch(err){
-        res.send(err.code+` / `+err.message);
+    } catch (err) {
+        res.send(err.code + ` / ` + err.message);
     }
 });
 
-//Borrar
-app.delete("/eliminar", async (req, res) =>{
-    try{
-        connection= await mysql.createConnection({ host: 'roundhouse.proxy.rlwy.net', user: 'root', database: 'railway', password: 'BJeuxTjJuwfhXSaolTreiTJqpiREPhAl' });
-        sql =`DELETE FROM personajes WHERE id = "${req.query.id}"`; 
-        var [rows,fields] = await connection.query(sql);
-        await connection.end();
-        res.status(201).json({ message: "Personaje eliminado con exito"});
-    } catch(err){
-        res.send(err.code+` / `+err.message);
-    }
-})
+// Borrar
+app.delete("/eliminar", async (req, res) => {
+    try {
+        const connection = await mysql.createConnection({
+            host: 'roundhouse.proxy.rlwy.net',
+            user: 'root',
+            database: 'railway',
+            password: 'BJeuxTjJuwfhXSaolTreiTJqpiREPhAl'
+        });
 
-app.listen(3000,()=>{
+        const sql = 'DELETE FROM personajes WHERE id = ?';
+        const [rows, fields] = await connection.execute(sql, [req.query.id]);
+        await connection.end();
+
+        res.status(201).json({ message: "Personaje eliminado con éxito" });
+    } catch (err) {
+        res.send(err.code + ` / ` + err.message);
+    }
+});
+
+app.listen(3000, () => {
     console.log("Servidor Express escuchando en puerto 3000");
 });
